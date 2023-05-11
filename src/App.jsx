8624d4632bf7b1458e12989dfbfdc4be251ec17c
927joanna <sot.joanna@gmail.com>
2023-05-11 22:19:52 +0800

    Updated base styling for App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import ChatPage from "./components/ChatPage";
+import ChatInput from "./components/ChatInput";
 
 function App() {
   return (
-    <div>
-      <h2>GA SEI</h2>
+    <div className='centered'>
+      <div>
+        <ChatPage />
+      </div>
     </div>
   );
 }
