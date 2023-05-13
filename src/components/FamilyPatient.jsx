eb7b133a689c1936627ebcc46047399d3ec8504c
Khairuzzaman <kzamanbm@gmail.com>
2023-05-14 01:55:53 +0800

    testing out logic for FamilyPatient

--- src/components/FamilyPatient.jsx
+++ src/components/FamilyPatient.jsx
@@ -1,56 +1,102 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import styles from "./FamilyPatient.module.css";
 import Header from "./Header";
 import { Avatar } from "@mui/material";
 import Patient1 from "./Images/Patient1.png";
+import { fetchData } from "../helpers/common";
 
 const FamilyPatient = () => {
+  const [patients, setPatients] = useState([]);
+  const [staffs, setStaffs] = useState([]);
+  const [showVerificationModal, setShowVerificationModal] = useState(false);
+
+  const getPatients = async () => {
+    const { ok, data } = await fetchData("/api/patients");
+
+    if (ok) {
+      setPatients(data);
+    } else {
+      console.log(data);
+    }
+  };
+  const getStaffs = async () => {
+    const { ok, data } = await fetchData("/api/staff");
+
+    if (ok) {
+      setStaffs(data);
+    } else {
+      console.log(data);
+    }
+  };
+
+  useEffect(() => {
+    getPatients();
+    getStaffs();
+  }, []);
+
   return (
-    <div className={styles.FamilyPatient}>
-      <Header />
-      <div className={styles.MyCareDisplay}>
-        <div className={styles.MyCare}>My Care</div>
-      </div>
-      <div className={styles.PatientDetailsDisplay}>
-        <div className={styles.PatientDetails}>
-          <Avatar
-            alt="Patient1"
-            src={Patient1}
-            sx={{ width: 96, height: 96 }}
-          />
-          <div className={styles.PatientDetailsBox}>
-            <div className={styles.PatientNameBox}>
-              <div className={styles.PatientName}>Patient Name</div>
-              <div className={styles.PatientNameText}>Kah Poh Tian</div>
-            </div>
-            <div className={styles.HospitalDetailsBox}>
-              <div className={styles.admitted}>Admitted To</div>
-              <div className={styles.hospName}>
-                Ang Mo Kio Community Hospital
+    <>
+      {showVerificationModal && <VerificationModal />}
+      <div className={styles.FamilyPatient}>
+        <Header />
+        <div className={styles.MyCareDisplay}>
+          <div className={styles.MyCare}>My Care</div>
+        </div>
+        <div className={styles.PatientDetailsDisplay}>
+          <div className={styles.PatientDetails}>
+            <Avatar
+              alt="Patient1"
+              src={Patient1}
+              sx={{ width: 96, height: 96 }}
+            />
+            <div className={styles.PatientDetailsBox}>
+              <div className={styles.PatientNameBox}>
+                <div className={styles.PatientName}>Patient Name</div>
+                <div className={styles.PatientNameText}>
+                  {/* {patient_firstName} */}
+                  {/* {patient_lastName} */}
+                  Kah Poh Tian
+                </div>
               </div>
-              <div className={styles.WardNoBedNo}>Ward 46 / Bed 1</div>
-            </div>
-            <div className={styles.StaffNameBox}>
-              <div className={styles.StaffName}>Staff Name</div>
-              <div className={styles.StaffNameText}>Staff Nurse Mabel See</div>
+              <div className={styles.HospitalDetailsBox}>
+                <div className={styles.admitted}>Admitted To</div>
+                <div className={styles.hospName}>
+                  Ang Mo Kio Community Hospital
+                </div>
+                <div className={styles.WardNoBedNo}>
+                  {/* {patient_ward} */}
+                  Ward 46 /{/* {patient_bed} */}
+                  Bed 1
+                </div>
+              </div>
+              <div className={styles.StaffNameBox}>
+                <div className={styles.StaffName}>Staff Name</div>
+                <div className={styles.StaffNameText}>
+                  {/* {staff_firstName} */}
+                  {/* {staff_lastName} */}
+                  Staff Nurse Mabel See
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles.ButtonBox}>
-          <div className={styles.Button2Box}>
-            <button className={styles.MessageStaffBox}>
-              <div className={styles.MessageStaffText}>Message Staff</div>
-            </button>
-            <div className={styles.badge}>
-              <div className={styles.badge1}>10</div>
+          <div className={styles.ButtonBox}>
+            <div className={styles.Button2Box}>
+              <button className={styles.MessageStaffBox}>
+                <div className={styles.MessageStaffText}>Message Staff</div>
+              </button>
+              <div className={styles.badge}>
+                <div className={styles.badge1}>10</div>
+              </div>
+              <button className={styles.PatientDetailsTextBox}>
+                <div className={styles.PatientDetailsText}>
+                  Patient's Details
+                </div>
+              </button>
             </div>
-            <button className={styles.PatientDetailsTextBox}>
-              <div className={styles.PatientDetailsText}>Patient's Details</div>
-            </button>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
