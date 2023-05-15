e91c6184904b2d769a46b57ef6e738781be3d448
927joanna <sot.joanna@gmail.com>
2023-05-15 12:12:48 +0800

    base fetch + left-right styling

--- src/components/ChatBody.jsx
+++ src/components/ChatBody.jsx
@@ -1,18 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './ChatBody.module.css'
 import ChatMessage from './ChatMessage'
+import { fetchData } from '../helpers/common'
 
 const ChatBody = () => {
-  const [messages, setMessages] = useState({})
+  const [messages, setMessages] = useState([])
 
+  const getMessages = async () => {
+    const { ok, data } = await fetchData('/api/chats')
+
+    if (ok) {
+      setMessages(data)
+    } else {
+      console.log(data)
+    }
+  }
+
+  useEffect(()=> {
+    getMessages
+  },[])
   
 
   return (
     <div className={styles['main-container']}>
+      <button onClick={getMessages}>a</button>
+      <button onClick={()=> console.log(messages)}>b</button>
       { messages.length > 0 &&
         // If messages exist, render all the messages
         messages.map((message)=> {
-          <ChatMessage message={message}/>
+          return (
+          <ChatMessage id={message._id} key={message._id} message={message}/>)
         })     
       }
     </div>
--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./ChatMessage.module.css";
+import UserContext from "../context/user"
 
 const ChatMessage = (props) => {
+  const userDetails = useContext(UserContext)
   const messageDetails = props.message
+
+  console.log('a', userDetails.userID)
 
   return (
     <div className={styles["main-container"]}>
-      {/* { (use context id == message sender id) ? className right-side : className left-side} */}
-      <div className={styles["message-details"]}>
-
-
-        <div className={styles["sender"]}>{messageDetails.sender}</div>
-
-
-        <div className={styles["message-text"]}>{props.messageContent}</div>
-        <div className={styles["time-stamp"]}>
-          <input
-            type="checkbox"
-            value="read" 
-            // it will be checked if isRead is true
-            checked={props.isRead}
-            // if isRead is true, the input will also be disabled
-            disabled={props.isRead}
-          />
-          {props.timeSent}
+      
+        <div className={styles["message-details"]} style={ messageDetails.msg_senderId === userDetails.userID ? { marginLeft: 'auto'} : {marginRight : 'auto'} }  >
+          <div className={styles["sender"]}>{messageDetails.msg_senderId}</div>
+          <div className={styles["message-text"]} style={ messageDetails.msg_senderId === userDetails.userID ? { marginLeft: 'auto'} : {marginRight : 'auto'} }>{messageDetails.msg_content}</div>
+          <div className={styles["time-stamp"]}>
+            <input
+              type="checkbox"
+              value="read" 
+              // it will be checked if isRead is true
+              checked={messageDetails.msg_isRead}
+              // if isRead is true, the input will also be disabled
+              disabled={props.isRead}
+            />
+            {props.timeSent}
+          </div>
         </div>
-      </div>
+
+      
     </div>
   );
 };
